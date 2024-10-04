f08a0dff2f86344cd684f11c63dbc0729bac92fb
asif1722 <asifantulay172@gmail.com>
2024-10-04 16:55:03 +0300

    fourth commit

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
 const apiKey = "665d758556dbc5ceebe90b1f214689bf";
-const apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=nairobi&units=metric";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric";
+const searchBtn = document.getElementById("search-btn");
 
 async function checkWeather() {
-    const response = await fetch(apiUrl + `&appid=${apiKey}`);
+    let city = document.getElementById("searchbox").value;
+    const response = await fetch(`${apiUrl}&appid=${apiKey}&q=${city}`);
     let data = await response.json();
     console.log(data);
 
     document.getElementById("city").innerText = data.name;
-    document.getElementById("temperature").innerText = `Temperature: ${data.main.temp}`;
+    document.getElementById("temperature").innerText = `Temperature: ${data.main.temp}°C`;
     document.getElementById("humidity").innerText = `Humidity: ${data.main.humidity}%`;
     document.getElementById("wind-speed").innerText = `Wind speed: ${data.wind.speed} km/h`;
 }
 
-checkWeather();
+searchBtn.onclick = checkWeather;
