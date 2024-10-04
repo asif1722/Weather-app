ab33853276ef3ca5c52c84bad7f8e5eb6fcfefa1
asif1722 <asifantulay172@gmail.com>
2024-10-04 16:29:27 +0300

    third commit

--- script.js
+++ script.js
@@ -5,6 +5,11 @@ async function checkWeather() {
     const response = await fetch(apiUrl + `&appid=${apiKey}`);
     let data = await response.json();
     console.log(data);
+
+    document.getElementById("city").innerText = data.name;
+    document.getElementById("temperature").innerText = `Temperature: ${data.main.temp}`;
+    document.getElementById("humidity").innerText = `Humidity: ${data.main.humidity}%`;
+    document.getElementById("wind-speed").innerText = `Wind speed: ${data.wind.speed} km/h`;
 }
 
 checkWeather();
